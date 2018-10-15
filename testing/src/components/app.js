cbf411f61f8278ec5f0039ba20d95a394e507ad7
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-10-15 09:31:52 +0800

    Initial commit of new testing app

---
+++ testing/src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app container">
+        <h4 className="app-header">Put more components here !</h4>
+      </div>
+    );
+  }
+}
+
+export default App;
