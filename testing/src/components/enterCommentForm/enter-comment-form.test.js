cc234e6ba34d0d32a83b9b6aded231259ab23d39
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-10-16 14:41:01 +0800

    Added test for testing form submit

--- testing/src/components/enterCommentForm/enter-comment-form.test.js
+++ testing/src/components/enterCommentForm/enter-comment-form.test.js
@@ -6,7 +6,7 @@ describe('Enter Comment Form', () => {
   let wrapped = null;
 
   beforeEach(() => {
-    wrapped = mount(<EnterCommentForm />);
+    wrapped = mount(<EnterCommentForm submitComment={()=>{}} />);
   });
 
   afterEach(() => {
@@ -18,8 +18,10 @@ describe('Enter Comment Form', () => {
     expect (wrapped.find(`button`).length).toEqual(1);
   });
 
-  it(`allows user to enter input on the textarea `, () => {
+  it(`allows user to enter value on the textarea field`, () => {
     const enteredInput = "Hello World !";
+
+    // Simulate change event on the textarea
     wrapped.find('textarea').simulate('change', { 
       target: {
         value: enteredInput
@@ -30,5 +32,31 @@ describe('Enter Comment Form', () => {
     wrapped.update();
     
     expect(wrapped.find('textarea').prop('value')).toEqual(enteredInput);
-  })
+  });
+
+  it(`empties textarea when the input is submitted`, ()=>{
+    const enteredInput = "Hello World !";
+
+    // Simulate change event on the textarea
+    wrapped.find('textarea').simulate('change', { 
+      target: {
+        value: enteredInput
+      }
+    });
+    
+    // force component to re-render
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual(enteredInput);
+
+    // Simulate form's on submit event
+    wrapped.find(`form`).simulate('submit', {
+      target: {
+        preventDefault: () => {}
+      }
+    });
+
+    // force component to re-render
+    wrapped.update();    
+    expect(wrapped.find('textarea').prop('value')).toEqual("");
+  });
 });
