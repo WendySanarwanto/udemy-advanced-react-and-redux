88cc9e8fecf601bb34e421e48a0d81a12dcb53e4
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-04-20 18:01:24 +0800

    Handled fetch_comments action in reducer

--- testing/src/reducers/comments.reducer.js
+++ testing/src/reducers/comments.reducer.js
@@ -1,5 +1,7 @@
-import { SUBMIT_COMMENT } from '../actions/index';
+import _ from 'lodash';
 import shortid from 'shortid';
+
+import { FETCH_COMMENTS, SUBMIT_COMMENT } from '../actions/index';
 
 export default function(state={}, action){
   switch(action.type){
@@ -13,7 +15,9 @@ export default function(state={}, action){
       };
       // console.log(`[DEBUG] - <comments.reducer> updatedState: \n`, updatedState);
       return updatedState;
-    
+    case FETCH_COMMENTS:
+      // console.log(`[debug]<FETCH_COMMENTS> action.payload.data: \n`, action.payload.data);
+      return {...state, ..._.mapKeys(action.payload.data, 'id')};
     default:
       return state;
   }
