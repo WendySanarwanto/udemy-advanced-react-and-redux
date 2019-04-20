73d9092019d93e5c2bfa44abab90e123c1b6ab62
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-04-20 18:01:06 +0800

    Added fetch_comments action creator

--- testing/src/actions/index.js
+++ testing/src/actions/index.js
@@ -1,7 +1,21 @@
-export const SUBMIT_COMMENT = 'submit_comment';
+import axios from 'axios';
+
+import { FETCH_COMMENTS, SUBMIT_COMMENT } from './types';
+export { FETCH_COMMENTS, SUBMIT_COMMENT };
+
+const API_HOST_URL = `http://jsonplaceholder.typicode.com`;
+
+export function fetchComments() {
+  const responsePromise = axios.get(`${API_HOST_URL}/comments`);
+
+  return {
+    type: FETCH_COMMENTS,
+    payload: responsePromise
+  }
+}
 
 export function submitComment(comment) {
-  // console.log(`[DEBUG] - <actions.submitComment> comment: ${comment}`);
+  // console.log(`[DEBUG] - <actions.submitComment> comment: ${comment}`);  
   return {
     type: SUBMIT_COMMENT,
     payload: comment
---
+++ testing/src/actions/types.js
@@ -0,0 +1,2 @@
+export const FETCH_COMMENTS = 'fetch_comments';
+export const SUBMIT_COMMENT = 'submit_comment';
