f7da191576c69c684d61d5be1df656f603f3ea45
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-04-21 09:52:18 +0800

    use https

--- testing/src/actions/index.js
+++ testing/src/actions/index.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 import { FETCH_COMMENTS, SUBMIT_COMMENT } from './types';
 export { FETCH_COMMENTS, SUBMIT_COMMENT };
 
-const API_HOST_URL = `http://jsonplaceholder.typicode.com`;
+const API_HOST_URL = `https://jsonplaceholder.typicode.com`;
 
 export function fetchComments() {
   const responsePromise = axios.get(`${API_HOST_URL}/comments`);
-
+  // console.log(`[debug]<fetchComments> responsePromise: \n`, responsePromise);
   return {
     type: FETCH_COMMENTS,
     payload: responsePromise
