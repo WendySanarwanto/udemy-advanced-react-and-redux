55e4a37259f8ceca2917a0fd2215639556cef0c7
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-04-21 19:45:50 +0800

    Refactored App component by adding navigation bar with signin/out button and also connect it to auth reducer and changeAuth action creator

--- testing/src/components/App.jsx
+++ testing/src/components/App.jsx
@@ -1,19 +1,59 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
+import { changeAuth } from '../actions';
 
-class App extends Component {
-  render() {
+const signInOutButtonStyle =  { marginRight: '2em' };
+
+const App = props => {
+  const renderButton = () => {
+    const isSignedIn = props.auth;
+    if (isSignedIn) {
+      return <button className="navbar-btn btn btn-primary" 
+                    style={ signInOutButtonStyle }
+                    onClick={ () => props.changeAuth(false) } >Sign Out</button>;
+    }
+
+    return <button className="navbar-btn btn btn-primary" 
+                   style={ signInOutButtonStyle }
+                   onClick={ () => props.changeAuth(true) }>Sign In</button>;
+  }
+  
+  const renderHeader = () => {
     return (
-      <div className="app container">
-        <Route path="/post" component={ CommentBox }/>
-        <Route path="/" exact component={ CommentList } />
-      </div>
+      <nav class="navbar navbar-default">
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/post">Post A Comment</Link>
+          </li>
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            { renderButton() }
+          </li>
+        </ul>
+      </nav>
     );
   }
+  
+  return (
+    <div className="app container">
+      { renderHeader() }
+      <Route path="/post" component={ CommentBox }/>
+      <Route path="/" exact component={ CommentList } />
+    </div>
+  );
 }
 
-export default App;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps, { changeAuth })(App);
