fedd0906b8b57ebb882991c69ca7530673d4e777
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-04-22 20:15:26 +0800

    Implement signIn

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -4,6 +4,7 @@ const User = require('../models/user');
 function tokenForUser(user, secret) {
   const timeStamp = new Date().getTime();
   // sub -> subject , iat = issued at time
+  // TODO: Should set exp in the option 
   return jwt.encode({ sub: user.id, iat: timeStamp }, secret);
 }
 
@@ -41,7 +42,7 @@ exports.signUp = async (req, res, next) => {
     });
 
     await newUser.save();
-    console.log(`[debug]<authentication@signUp> newUser: \n`, newUser);
+    // console.log(`[debug]<authentication@signUp> newUser: \n`, newUser);
 
     // Respond to request indicating the user was created
     res.json({ token: tokenForUser(newUser, jwtSecret) });
@@ -50,3 +51,13 @@ exports.signUp = async (req, res, next) => {
     return next(err);
   }
 };
+
+exports.signIn = async (req, res, next) => {
+  // User has already had their email and password auth'd
+  // We just need to give them the token.
+  // Local login strategy passed in the matched User data into req
+  const user = req.user;
+  // console.log(`[debug]<authentication@signIn> user: \n`, user);
+  const jwtSecret = process.env.EXPRESS_AUTH_API_JWT_SECRET_KEY;
+  res.send({ token: tokenForUser(user, jwtSecret) });
+}
--- auth/server/router.js
+++ auth/server/router.js
@@ -4,11 +4,12 @@ const passport = require('passport');
 
 // auth middleware - session = false, means we don't want to use cookie
 const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = (app) => {
   app.get('/', requireAuth, (req, res) => {
     res.send({ hi: 'there' })
   });
+  app.post('/signin', requireSignin, Authentication.signIn);
   app.post('/signup', Authentication.signUp);
-  
 };
