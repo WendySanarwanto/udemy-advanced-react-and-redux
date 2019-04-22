2b53db65a999e06de900ea15706755f06b99c818
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-04-22 09:05:55 +0800

    Fixed coding error on async.js custom middleware

--- testing/src/middlewares/async.js
+++ testing/src/middlewares/async.js
@@ -2,7 +2,7 @@ export default ({ dispatch }) => next => action => {
   // Check to see if the payload has a promise on its 'payload' property
   // If it does, wait for it to resolve
   // If it doesn't, send the action on to next middleware
-  if ( (!action.payload) && (action.payload.then) ) {
+  if ( (!action.payload) || (!action.payload.then) ) {
     return next(action);
   }
 
