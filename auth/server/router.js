71e86a953d36e2e2d89f665c6ebeaaf002929aaf
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-04-22 19:22:22 +0800

    IMported passportService and created requireAuth middleware

--- auth/server/router.js
+++ auth/server/router.js
@@ -1,5 +1,14 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// auth middleware - session = false, means we don't want to use cookie
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = (app) => {
+  app.get('/', requireAuth, (req, res) => {
+    res.send({ hi: 'there' })
+  });
   app.post('/signup', Authentication.signUp);
+  
 };
