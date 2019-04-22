a5833d93f272eb8f3908ef081b882b26bb64c949
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-04-22 15:01:17 +0800

    import router and  connection to mongodb auth database

--- auth/server/index.js
+++ auth/server/index.js
@@ -3,13 +3,21 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const http = require('http');
+const mongoose = require('mongoose');
 
+const router = require('./router');
 const app = express();
+
+// DB Setup
+mongoose.connect(`mongodb://localhost:auth/auth`, { 
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
 
 // App Setup
 app.use(morgan('combined')); // Logging framework
 app.use(bodyParser.json({ type: '*/*' }));
-
+router(app);
 
 // Server setup
 const port = process.env.PORT || 3090;
