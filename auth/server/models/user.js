373d07482fe8a280215fd959dd9d926360dcffbe
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-04-22 16:33:48 +0800

    Added pre-save hook to hash the plain text password by using bcrypt hash

--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define our model
 const userSchema = new Schema({
@@ -17,6 +18,33 @@ const userSchema = new Schema({
     type: Number,
     default: 0
   }
+});
+
+// On Save Hook, encrypt password
+userSchema.pre('save', function(next) {
+  // Get access to the user model's instance
+  const user = this;
+  // console.log(`[debug]<UserSchema@pre> user: \n`, user);
+
+  // Generate a salt
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      console.error(err);
+      return next(err);
+    }
+
+    // Hash the password (encrypt) using the salt
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) {
+        console.error(err);
+        return next(err);
+      }
+      
+      // Overwrite plain text password with encrypted password
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 // Create the model class
