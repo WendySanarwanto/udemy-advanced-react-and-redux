c0b729f34737d3ecc73805c0b655af905f60965e
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-04-23 13:44:03 +0800

    Handle AuthForm's onSubmit callback with console.log

--- auth/client/src/components/auth/SignIn.jsx
+++ auth/client/src/components/auth/SignIn.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 import AuthForm from './AuthForm';
 
 const SignIn = () => {
+  const onSubmit = (formValues) => {
+    console.log(`[debug]<SignIn@onSubmit> formValues: \n`, formValues);
+    // TODO: Call signIn action creator
+  };
+
   return (
-    <AuthForm />
+    <div>
+      <h4>Sign In</h4>
+      <br />
+      <AuthForm onSubmit={ onSubmit }/>
+    </div>
   );
 };
 
--- auth/client/src/components/auth/SignUp.jsx
+++ auth/client/src/components/auth/SignUp.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 import AuthForm from './AuthForm';
 
 const SignUp = () => {
+  const onSubmit = (formValues) => {
+    console.log(`[debug]<SignUp@onSubmit> formValues: \n`, formValues);
+    // TODO: Call signup action creator
+  };
+  
   return (
-    <AuthForm />
+    <div>
+      <h4>Sign Up</h4>
+      <br />    
+      <AuthForm onSubmit={ onSubmit }/>
+    </div>
   );
 };
 
