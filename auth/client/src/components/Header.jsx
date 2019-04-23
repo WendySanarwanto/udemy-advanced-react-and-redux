4483d04233193b10250a911c0ba64d52443c84bb
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-04-24 07:18:43 +0800

    Hide show links based on authenticated state

--- auth/client/src/components/Header.jsx
+++ auth/client/src/components/Header.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Header = () => {
+const Header = props => {
+  const renderLinks = () => {
+    const isAuthenticated = props.isAuthenticated;
+
+    if (isAuthenticated) {
+      return (
+        <React.Fragment>
+          <li><Link to="/signout">Sign Out</Link></li>
+          <li><Link to="/feature">Feature</Link></li>
+        </React.Fragment>
+      );
+    }
+
+    return (
+      <React.Fragment>
+        <li><Link to="/signup">Sign Up</Link></li>
+        <li><Link to="/signin">Sign In</Link></li>
+      </React.Fragment>
+    );
+  };
+
   return (
     <nav className="navbar navbar-inverse">
       <div className="container-fluid">      
         <ul className="nav navbar-nav">
           <li><Link to="/">Redux Auth</Link></li>
-          <li><Link to="/signup">Sign Up</Link></li>
-          <li><Link to="/signin">Sign In</Link></li>
-          <li><Link to="/signout">Sign Out</Link></li>
-          <li><Link to="/feature">Feature</Link></li>
+          { renderLinks() }
         </ul>
       </div>
     </nav>
   );
 };
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+   isAuthenticated: state.auth && ((typeof state.auth.token) === 'string') && (state.auth.token.trim() !== '')
+  };
+};
+
+export default connect(mapStateToProps)(Header);
