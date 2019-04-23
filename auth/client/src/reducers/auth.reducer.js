c1b101bf629fbd44e9776e158ea9dee9ea123cce
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-04-23 14:40:15 +0800

    Handled SIGN_UP action type and update the state when response's code is 200

--- auth/client/src/reducers/auth.reducer.js
+++ auth/client/src/reducers/auth.reducer.js
@@ -1,10 +1,22 @@
+import { SIGN_UP } from '../actions/types';
+
 const INITIAL_STATE = {
   token: '',
-  errorMesasge: ''
+  errorMessage: ''
 };
 
 const authReducer = (state = INITIAL_STATE, action) => {
   switch(action.type){
+    case SIGN_UP:
+      let token = '';
+      if (action.payload.code === 200) {
+        token = action.payload.data.token;
+        return {...state, token};
+      }
+
+      // TODO: Handle error message 
+
+      return {state};
     default:
       return state;
   }
